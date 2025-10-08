4434e3a074cc61de8ab523dfd8fa6385e252d462
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-10-08 23:27:45 +0530

    Login Component

---
+++ frontend/src/Components/LoginSignup/Login.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Login.css" 
+
+const Login = () => {
+    return(
+        <div className="login-container">
+        <h1 className="header">Log In to TruthLab</h1>
+
+        <form> 
+            <div className="input-group">
+                <label htmlFor="email">Email address</label>
+                <div className="input-field-container">
+                    <i className="fa-regular fa-user"></i>
+                    <input type="email" id="email" placeholder="Enter your email" required />
+                </div>
+            </div>
+
+            <div className="input-group"> 
+                <label htmlFor="password">Password</label>
+                <div className="input-field-container">
+                    <i className="fa-solid fa-key"></i>
+                    <input type="password" id="password" placeholder="Enter your password" required />
+                </div>
+            </div>
+
+            <a href="#" className="forgot-password">Forgot Password?</a>
+
+            <button type="submit" className="btn primary-btn">Log in</button>
+            <button type="button" className="btn secondary-btn">
+                <img
+                    src="https://www.svgrepo.com/show/475656/google-color.svg"
+                    alt="Google logo"
+                    className="google-icon"
+                />
+                Login with Google
+            </button>
+        </form>
+
+        <p className="signup-text">
+            Don't have an account? <a href="#" className="signup-link">Sign up</a>
+        </p>
+    </div>
+    )
+}
+
+
+export default Login
