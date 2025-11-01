405360dfb14a19f2dccd2332f5fbdcff5b6d695c
Pabasara Weerasooriya <pdewmini2002@gmail.com>
2025-11-01 09:57:30 +0530

    Added components for about page

---
+++ frontend/src/components/SectionHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../pages/css/About.css";
+
+const SectionHeader = () => {
+  return (
+    <section className="about-header">
+      <h1>About TRUTHLAB</h1>
+      <p>
+        We’re on a mission to combat misinformation and empower people with the
+        tools to identify reliable information.
+      </p>
+    </section>
+  );
+};
+
+export default SectionHeader;
