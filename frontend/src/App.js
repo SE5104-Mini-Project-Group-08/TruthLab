01bb5d07667ff0aa1aba31e05be1d9cfb74d77d3
Kaveesha Sewmini <kaveeshasewmini161@gmail.com>
2026-02-22 22:20:24 +0530

    front end routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Login from './pages/LoginSignup/Login'; 
 import Signup from './pages/LoginSignup/Signup';  
 import {Routes, Route} from "react-router-dom";
+import About from './pages/About';
+import VerifyPage from './pages/VerifyPage';
+import FAQ from './pages/FAQ';
 
 function App() {
   return (
@@ -9,6 +12,10 @@ function App() {
        <Routes>
             <Route path="/" element={<Login/>} />
             <Route path="/signup" element={<Signup/>} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/verify" element={<PrivateRoute><VerifyPage /></PrivateRoute>} />
+            <Route path="/faq" element={<FAQ />} />
         </Routes>
     </div>
   );
